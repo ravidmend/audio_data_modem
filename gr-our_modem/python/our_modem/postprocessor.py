e5ba07e1c64d3924ed2a78680be988c152747cf0
Ravidmend <Ravid1475@gmail.com>
2026-03-05 10:22:39 +0200

    BASIC VERION WORKS with queueing the input

--- gr-our_modem/python/our_modem/postprocessor.py
+++ gr-our_modem/python/our_modem/postprocessor.py
@@ -9,31 +9,155 @@
 
 import numpy as np
 from gnuradio import gr
+from scipy.signal import find_peaks
+from collections import deque
 
 class postprocessor(gr.sync_block):
     """
     docstring for block postprocessor
     """
+
     def __init__(self, t,fs,sensitivity,timeout):
         self.t = t
         self.fs = fs
         self.sensitivity = sensitivity
         self.timeout = timeout
+        self.bits = []
+        self.queue = deque([])
+        self.did_removed_preamble = False
         gr.sync_block.__init__(self,
             name="postprocessor",
             in_sig=[np.float32, ],
             out_sig=None)
 
 
-    def work(self, input_items, output_items):
+    def ravid_work(self, input_items, output_items):
         in0 = input_items[0]
+        print(f"input samples:{input_items[0]}")
         # <+signal processing here+>
-        diff = np.diff(input_items[0])
+        sps = int(self.t*self.fs)
+
+        window = np.concatenate(np.full(sps, 1), np.full(sps, -1))
+        diff = np.convolve(in0, window, mode='full')
 
 
+        threshold = 0.15 * 2 * sps * 0.5
 
+        # threshold = diff[sps]
+        bits = []
+        for peak in diff[3*sps:len(diff):3*sps]:
+            bit = 1
+            if peak>threshold:
+                bit = 0
+            bits.append(bit)
+
+        #print(bits[0:min(100, len(bits)-1)])
+        output_str = self.bits_to_string(bits)
+        
+        print(output_str)
+            
         return len(input_items[0])
     
+
+    def old_work(self, input_items, output_items):
+        in0 = input_items[0]
+        window = np.concatenate((np.full((int(self.t*self.fs)), 1), np.full((int(self.t*self.fs)), -1)))
+        diff = np.correlate(in0, window, mode='full')
+
+        peak_indices, _ = find_peaks(diff)
+        print(peak_indices[0:min(100, len(peak_indices)-1)])
+
+        peak_indices = np.concatenate(([0], peak_indices))
+        
+        
+        for peak_idx in peak_indices:
+            
+            if peak_idx < len(diff):
+                bit = 1
+                if input_items[0][int(peak_idx + 0.5*int(self.t*self.fs))] < 0:
+                    bit = 0
+                self.bits.append(bit)
+            
+            if (len(self.bits) == 8):
+                output_str = self.bits_to_string(self.bits)
+                print(f"char is {output_str}")
+                self.bits = []
+            
+        return len(input_items[0])
+
+
+    def BABOON_work(self, input_items, output_items):
+        in0 = input_items[0]
+        sps = int(self.t*self.fs)
+        
+        for x in input_items[0]:
+            if (x>0):
+                x=1
+            else:
+                x=-1
+
+        print(f"size:{len(in0)},first_sample:{in0[0:30]}")
+        diff = np.diff(in0)
+
+        # peak_indices, _ = find_peaks(diff)
+        # print(peak_indices[0:min(100, len(peak_indices)-1)])
+
+        # peak_indices = np.concatenate(([0], peak_indices))
+        
+        
+        for index,dif in enumerate(diff):
+            if(dif == -2):
+                # print(f"index is{index}")
+                bit = 1
+                # for i in range(3,(int(1.3*sps))):
+                #     if((index + i)<len(in0) and in0[index + i] != -1):
+                #         bit = 1
+                #         break
+                if(in0[index -(sps+1)] == -1):
+                    bit = 0
+
+                
+                self.bits.append(bit)
+            
+            if (len(self.bits) == 8):
+                output_str = self.bits_to_string(self.bits)
+                print(f"char is {output_str}")
+                self.bits = []
+            
+        return len(input_items[0])
+    
+    def work(self, input_items, output_items):
+        in0 = input_items[0]
+        sps = int(self.t*self.fs)
+        self.queue.extend(in0)
+        if (self.did_removed_preamble == False):
+            # for i in range(sps): 
+            while(True):
+                if(self.queue[0] == -1):
+                    self.queue.popleft()
+                else:
+                    break
+            self.did_removed_preamble = True
+
+        while(len(self.queue) > (3 * sps)):
+            dequeued_items = np.array([self.queue.popleft() for _ in range(3 * sps)])
+            bit = postprocessor.decide_bit(dequeued_items,sps)
+            self.bits.append(bit)
+
+            if (len(self.bits) == 8):
+                output_str = self.bits_to_string(self.bits)
+                print(f"{output_str}")
+                self.bits = []
+        
+        # for x in input_items[0]:
+        #     if (x>0):
+        #         x=1
+        #     else:
+        #         x=-1
+
+            
+        return len(input_items[0])
+
     @staticmethod
     def bits_to_string(bits_array):
         output_str = ""
@@ -44,3 +168,11 @@ class postprocessor(gr.sync_block):
             ch = int(byte_string,2)
             output_str += chr(ch)
         return output_str
+
+    @staticmethod
+    def decide_bit(samples_array,sps):
+        if (samples_array[int(1.5*sps)] == 1):
+            return 1
+        else:
+            return 0
+
--- gr-our_modem/transmitter.py
+++ gr-our_modem/transmitter.py
@@ -11,7 +11,6 @@
 
 from PyQt5 import Qt
 from gnuradio import qtgui
-from gnuradio import analog
 from gnuradio import blocks
 from gnuradio import gr
 from gnuradio.filter import firdes
@@ -23,7 +22,6 @@ from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
 from gnuradio import our_modem
-import sip
 
 
 
@@ -62,51 +60,23 @@ class transmitter(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
+        self.t = t = 0.001
         self.samp_rate = samp_rate = 32000
+        self.my_string = my_string = "abcde"*100
 
         ##################################################
         # Blocks
         ##################################################
 
-        self.qtgui_sink_x_0_0 = qtgui.sink_f(
-            1024, #fftsize
-            window.WIN_BLACKMAN_hARRIS, #wintype
-            0, #fc
-            samp_rate, #bw
-            "", #name
-            True, #plotfreq
-            True, #plotwaterfall
-            True, #plottime
-            True, #plotconst
-            None # parent
-        )
-        self.qtgui_sink_x_0_0.set_update_time(1.0/10)
-        self._qtgui_sink_x_0_0_win = sip.wrapinstance(self.qtgui_sink_x_0_0.qwidget(), Qt.QWidget)
-
-        self.qtgui_sink_x_0_0.enable_rf_freq(False)
-
-        self.top_layout.addWidget(self._qtgui_sink_x_0_0_win)
-        self.our_modem_preprocess_0 = our_modem.preprocess(0.1, samp_rate, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        self.our_modem_preprocess_0 = our_modem.preprocess(t, samp_rate, my_string)
+        self.our_modem_postprocessor_0_0_0 = our_modem.postprocessor(t, samp_rate, 0.1, 0.333)
         self.blocks_throttle2_0 = blocks.throttle( gr.sizeof_float*1, samp_rate, True, 0 if "auto" == "auto" else max( int(float(0.1) * samp_rate) if "auto" == "time" else int(0.1), 1) )
-        self.analog_wfm_tx_0 = analog.wfm_tx(
-        	audio_rate=samp_rate,
-        	quad_rate=samp_rate,
-        	tau=(75e-6),
-        	max_dev=75e3,
-        	fh=(-1.0),
-        )
-        self.analog_wfm_rcv_0 = analog.wfm_rcv(
-        	quad_rate=samp_rate,
-        	audio_decimation=1,
-        )
 
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_wfm_rcv_0, 0), (self.qtgui_sink_x_0_0, 0))
-        self.connect((self.analog_wfm_tx_0, 0), (self.analog_wfm_rcv_0, 0))
-        self.connect((self.blocks_throttle2_0, 0), (self.analog_wfm_tx_0, 0))
+        self.connect((self.blocks_throttle2_0, 0), (self.our_modem_postprocessor_0_0_0, 0))
         self.connect((self.our_modem_preprocess_0, 0), (self.blocks_throttle2_0, 0))
 
 
@@ -118,13 +88,24 @@ class transmitter(gr.top_block, Qt.QWidget):
 
         event.accept()
 
+    def get_t(self):
+        return self.t
+
+    def set_t(self, t):
+        self.t = t
+
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
         self.blocks_throttle2_0.set_sample_rate(self.samp_rate)
-        self.qtgui_sink_x_0_0.set_frequency_range(0, self.samp_rate)
+
+    def get_my_string(self):
+        return self.my_string
+
+    def set_my_string(self, my_string):
+        self.my_string = my_string
 
 
 
